672eb2d3cd489a592a3fa0fb4dc85253a223bbda
wathika <vcwathika@gmail.com>
2015-08-21 00:41:09 +0300

    commented

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "cuba"  #First, you load the Cuba gem to access its functionality.
 require "mote"
 require "mote/render"
 
-Cuba.plugin(Mote::Render)
+Cuba.plugin(Mote::Render) #This plugin tells Cuba where to look for template files and uses Mote to render them.
 
 Cuba.define do  #define => allows us to create an application through a block
   on root do   #on => executes a given block if the passed conditions evaluate to true.
